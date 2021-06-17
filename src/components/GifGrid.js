ddf7ed7c960fa6adaae829007cf2b0863ab1ff30
robertgrc <robert.rodriguez.rgrc@gmail.com>
2021-06-16 22:47:35 -0400

    custom hooks

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,21 @@
-import React, { useEffect, useState} from 'react'
-import { getGifs } from '../helpers/getGifs';
-import { GifGridItem } from './GifGridItem';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+// import { getGifs } from '../helpers/getGifs';
+// import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
 
-    const [images, setImages] = useState([])
+    // const [images, setImages] = useState([])
 
-    useEffect(() => {
-        getGifs ( category )
-            .then( setImages );
-    }, [category])
+    const { loading } = useFetchGifs();
+
+    console.log(loading);
+    
+    // useEffect(() => {
+    //     getGifs ( category )
+    //         .then( setImages );
+    // }, [category])
 
     // getGifs es llamado ahora dentro de useEffect para que solo se ejecute una vez 
     //getGifs();
@@ -18,7 +23,8 @@ export const GifGrid = ({category}) => {
     return (
         <div>
             <h3>{ category }</h3>
-            
+            {loading ? 'Cargando': 'data cargada'}
+            {/*             
                 {
                     images.map( img => (
                         <GifGridItem 
@@ -27,7 +33,7 @@ export const GifGrid = ({category}) => {
                         />
                     ))
                 }
-            
+             */}
         </div>
     )
 }
